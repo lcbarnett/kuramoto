61f8fc6a8d5a7fc85a4ef90f4e2eadecab3f81d4
Lionel Barnett <l.c.barnett@sussex.ac.uk>
2022-08-14 17:12:16 +0100

    Amended test routine

--- Matlab/kuramoto_test.m
+++ Matlab/kuramoto_test.m
@@ -1,9 +1,9 @@
 
-N = 10;
-w = 0.5*randn(N,1);
-K = 0.02;
+N  = 10;
+w  = 0.5*randn(N,1);
+K  = 1/N;
 h0 = pi*(2*rand(N,1)-1);
-T = 100;
+T  = 100;
 dt = 0.01;
 
 fprintf('\n');
@@ -11,12 +11,12 @@ fprintf('\n');
 st1 = tic;
 [h1,r1,psi1,T,n] = kuramoto(N,w,K,h0,T,dt,false);
 et1 = toc(st1);
-fprintf('Euler method : %g\n',et1);
+fprintf('Euler method : %g seconds\n',et1);
 
 st2 = tic;
 [h2,r2,psi2,T,n] = kuramoto(N,w,K,h0,T,dt,true);
 et2 = toc(st2);
-fprintf('Runge-Kutta  : %g\n',et2);
+fprintf('Runge-Kutta  : %g seconds\n',et2);
 
 mad = max(abs(r1-r2));
 fprintf('\nMaximum absolute difference = %g\n\n',mad);
