bacdf9191438b17f43adc4729b4bbf5299422a5b
Lionel Barnett <l.c.barnett@sussex.ac.uk>
2023-01-30 11:47:30 +0000

    Fixed frequencies in audio sim

--- C/demo/audio.c
+++ C/demo/audio.c
@@ -6,6 +6,9 @@
 #include "kutils.h"
 #include "kuramoto.h"
 
+
+#define TWOPI    (2.0*M_PI)
+#define CD_SRATE 44100.0
 #define MIDDLE_C 261.625565
 
 // Program to demonstrate usage of Kuramoto C library.
@@ -20,39 +23,33 @@ int audio(int argc, char *argv[])
 	// Arg:  name    type    default    description
 	puts("\n---------------------------------------------------------------------------------------");
 	CLAP_ARG(N,      size_t, 4,         "number of oscillators");
-	CLAP_ARG(T,      double, 1.0,       "total integration time (secs)");
-	CLAP_ARG(f,      double, 44100.0,   "sampling frequency (Hz)");
-	CLAP_ARG(b,      int,    24,        "sampling bits");
-	CLAP_ARG(dt,     double, 1.0/f,     "integration step size");
-	CLAP_ARG(wmean,  double, MIDDLE_C,  "oscillator frequencies mean (Hz)");
-	CLAP_ARG(wrdev,  double, 0.5,       "oscillator frequencies rel. dev.");
-	CLAP_ARG(Kmean,  double, 0.1*f/N,   "coupling constants mean (Hz)");
-	CLAP_ARG(Krdev,  double, 1.0,       "coupling constants rel. dev.");
-	CLAP_ARG(Isdev,  double, 0.1,       "input noise intensity (zero for deterministic)");
+	CLAP_ARG(T,      double, 200.0,     "total integration time");
+	CLAP_ARG(f,      double, 1000.0,    "sampling frequency (Hz)");
+	CLAP_ARG(wmean,  double, 0.0,       "oscillator frequencies mean (Hz)");
+	CLAP_ARG(wsdev,  double, 20,        "oscillator frequencies std. dev. (Hz)");
+	CLAP_ARG(Kmean,  double, 8.0/N,     "coupling constants mean (Hz)");
+	CLAP_ARG(Ksdev,  double, Kmean/8.0, "coupling constants std. dev. (Hz)");
+	CLAP_ARG(Isdev,  double, 0.2,       "input noise intensity (Hz: zero for deterministic)");
 	CLAP_ARG(rseed,  uint,   0,         "random seed (or 0 for random random seed)");
 #ifdef _HAVE_GNUPLOT
 	CLAP_ARG(gpterm, cstr,   GPTERM,    "Gnuplot terminal type (if available)");
 #endif
-	puts("---------------------------------------------------------------------------------------\n");
+	puts("---------------------------------------------------------------------------------------");
 
 	// seed random number generator (from command line if you want predictability)
 
 	const uint seed = rseed > 0 ? rseed : get_rand_seed();
-	printf("random seed = %u\n\n",seed);
+	printf("\nrandom seed = %u\n\n",seed);
 	srand(seed);
 
-//	const double rad2Hz = f/(2.0*M_PI);
-	const double Hz2rad = (2.0*M_PI)/f;
+	// number of integration steps (sampling frequency should do)
 
-	// number of integration steps
-
-	const size_t n  = (size_t)ceil(T/dt);         // number of integration steps
-	printf("integration steps = %zu\n\n",n);
+	const double dt = 1.0/f;
+	const size_t n  = (size_t)ceil(T*f);         // number of integration steps
 
 	// allocate memory
 
 	double* const w = calloc(N,  sizeof(double)); // oscillator frequencies
-	double* const a = calloc(N*N,sizeof(double)); // coupling constants
 	double* const K = calloc(N*N,sizeof(double)); // coupling constants
 	double* const h = calloc(N*n,sizeof(double)); // oscillator phases, unwrapped
 	double* const r = calloc(n,  sizeof(double)); // order parameter
@@ -60,20 +57,7 @@ int audio(int argc, char *argv[])
 	// random frequencies (normal distribution)
 
 	for (size_t i=0; i<N; ++i) {
-		w[i] = dt*Hz2rad*wmean*(1.0+wrdev*randn()); // scale frequencies by dt
-	}
-
-	// random phase shifts (uniform distribution)
-
-	for (size_t i=0; i<N; ++i) {
-		for (size_t j=0; j<N; ++j) {
-			if (i == j) {
-				a[N*i+j] = 0.0; // no "self-connections"!
-			}
-			else {
-				a[N*i+j] = 0.01*2.0*M_PI*randu();
-			}
-		}
+		w[i] = TWOPI*dt*(wmean+wsdev*randn()); // scale frequencies by dt
 	}
 
 	// random coupling constants (normal distribution)
@@ -84,7 +68,7 @@ int audio(int argc, char *argv[])
 				K[N*i+j] = 0.0;                      // no "self-connections"!
 			}
 			else {
-				K[N*i+j] = dt*Hz2rad*Kmean*(1.0+Krdev*randn()); // scale coupling constants by dt
+				K[N*i+j] = TWOPI*dt*(Kmean+Ksdev*randn()); // scale coupling constants by dt
 			}
 		}
 	}
@@ -94,7 +78,7 @@ int audio(int argc, char *argv[])
 	const double sqrtdt = sqrt(dt);
 	if (Isdev > 0.0) {
 		for (size_t k=0; k<N*n; ++k) {
-			h[k] = sqrtdt*Hz2rad*Isdev*randn(); // scale input by sqrt(dt) [cf. Ornstein-Uhlenbeck process]
+			h[k] = TWOPI*sqrtdt*Isdev*randn(); // scale input by sqrt(dt) [cf. Ornstein-Uhlenbeck process]
 		}
 	}
 	else {
@@ -106,11 +90,11 @@ int audio(int argc, char *argv[])
 	const int RK4 = 0; // flag for RK4 (else Euler)
 	if (RK4) {
 		double* const kbuff = calloc(4*N,sizeof(double)); // see kuramoto_rk4()
-		kuramoto_rk4pl(N,n,w,K,a,h,kbuff);
+		kuramoto_rk4(N,n,w,K,h,kbuff);
 		free(kbuff);
 	}
 	else {
-		kuramoto_eulerpl(N,n,w,K,a,h);
+		kuramoto_euler(N,n,w,K,h);
 	}
 
 	// calculate order parameter
@@ -133,7 +117,7 @@ int audio(int argc, char *argv[])
 		fprintf(fp,"%17.8f",(double)(k+1)*dt);   // time stamp
 		fprintf(fp," %17.8f",r[k]);              // order parameter
 		for (size_t i=0; i<N; ++i) {
-			fprintf(fp," %17.8f",sin(f*h[N*k+i])); // oscillator signal (waveform)
+			fprintf(fp," %17.8f",sin(h[N*k+i])); // oscillator signal (waveform)
 		}
 		fprintf(fp,"\n");
 	}
@@ -153,7 +137,7 @@ int audio(int argc, char *argv[])
 		return EXIT_FAILURE;
 	}
 	fprintf(gp,"set term \"%s\" title \"Kuramoto oscillator demo\" size 1600,800\n",gpterm);
-	fprintf(gp,"set xlabel \"time\"\n");
+	fprintf(gp,"set xlabel \"time (secs)\"\n");
 	fprintf(gp,"set ylabel \"mean phase\"\n");
 	fprintf(gp,"set key right bottom Left rev\n");
 	fprintf(gp,"# set grid\n");
@@ -190,7 +174,6 @@ int audio(int argc, char *argv[])
 	free(r);
 	free(h);
 	free(K);
-	free(a);
 	free(w);
 
 	return EXIT_SUCCESS;
