2ad852f0eaac47a48e41ca7960dbe02ae0ad950c
lcbarnett <l.c.barnett@sussex.ac.uk>
2023-06-21 16:28:41 +0100

    Edited startup file

--- Matlab/startup.m
+++ Matlab/startup.m
@@ -1,3 +1,5 @@
+fprintf('[kuramoto startup] Initialising kuramoto Matlab interface\n');
+
 % Set up some global variables
 
 global seqno CD_SRATE MIDDLE_C
@@ -5,3 +7,6 @@ global seqno CD_SRATE MIDDLE_C
 seqno = 0;
 CD_SRATE = 44100;
 MIDDLE_C = 261.625565;
+
+fprintf('[kuramoto startup] Global variables set\n');
+fprintf('[kuramoto startup] Initialised (you may re-run `startup'' at any time)\n');
