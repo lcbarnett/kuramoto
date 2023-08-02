268a0724050c85b059355854b7e8071fd4d33b9c
Lionel Barnett <l.c.barnett@sussex.ac.uk>
2023-08-02 17:37:54 +0100

    Tweaks

--- Matlab/kuramutto/kmnois.m
+++ Matlab/kuramutto/kmnois.m
@@ -1,21 +1,24 @@
-% Uncorrelated Gaussian white inoise input to tail, magnitudes log-normally distributed
+% Uncorrelated Gaussian white inoise input to legs, magnitudes log-normally distributed
 
 % Default parameters (override on command line - see 'defvar.m')
 
-defvar('mmean', 0.01    ); % oscillator input inoise magnitude mean (zero for no inoise)
-defvar('msdev', mmean/5 ); % oscillator input inoise magnitude std. dev.
-defvar('mseed', []      ); % oscillator input inoise magnitude random seed (empty for no seeding)
-defvar('nseed', []      ); % oscillator input inoise random seed (empty for no seeding)
+defvar('mmean', 0.01    ); % oscillator input noise magnitude mean (zero for no inoise)
+defvar('msdev', mmean/5 ); % oscillator input noise magnitude std. dev.
+defvar('mseed', []      ); % oscillator input noise magnitude random seed (empty for no seeding)
+defvar('nseed', []      ); % oscillator input noise random seed (empty for no seeding)
+
+ilegs = [ifleg ihleg];
+nlegs = nfleg+nhleg;
 
 if mmean > 0 % with input inoise
 	if ~isempty(mseed), rstate = rng(mseed); end
 	nln = log(1+msdev^2/mmean^2);
-	nmag = (lognrnd(log(mmean)-nln/2,sqrt(nln),1,ntail));
+	nmag = (lognrnd(log(mmean)-nln/2,sqrt(nln),1,nlegs));
 	if ~isempty(mseed), rng(rstate); end
 
 	if ~isempty(nseed), rstate = rng(nseed); end
 	inoise = zeros(ndt,nosc);
-	inoise(:,itail) = nmag.*randn(ndt,ntail);
+	inoise(:,ilegs) = nmag.*randn(ndt,nlegs);
 	if ~isempty(nseed), rng(rstate); end
 else
 	inoise = []; % no inoise
